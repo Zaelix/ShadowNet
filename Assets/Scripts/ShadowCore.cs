a53c73d2fc7d0023419e90423e0f7b3e448ffb4a
Michael Johnson <zaelix@gmail.com>
2017-09-26 20:02:33 -0700

    Added Unity Project files

---
+++ Assets/Scripts/ShadowCore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowCore : MonoBehaviour {
+    public Canvas canvas;
+    private bool isMusicEnabled = true;
+    private AudioSource aSource;
+	// Use this for initialization
+	void Start () {
+        aSource = GetComponent<AudioSource>();
+        //aSource.mute = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isMusicEnabled)
+        {
+            //aSource.mute = false;
+        }
+        else
+        {
+            //aSource.mute = true;
+        }
+    }
+
+    private void ToggleMusic()
+    {
+        isMusicEnabled = canvas.GetComponentInChildren<Toggle>().isOn;
+    }
+}
